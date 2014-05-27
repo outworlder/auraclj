2da8f319f4d7cbb42cbfa6fa22a5aba341424769
Stephen Eilert <spedrosa@gmail.com>
2014-05-27 19:59:33 -0300

    Basic webservice http requests. Now, to figure out XML parsing.

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/data.zip "0.1.1"]
+                 [clj-http "0.9.2"]]
   :main ^:skip-aot auraclj.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/auraclj/core.clj
+++ src/auraclj/core.clj
@@ -1,7 +1,33 @@
 (ns auraclj.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clj-http.client :as client]
+            [clojure.xml :as xml]
+            [clojure.zip :as zip])
+  (:use [clojure.data.zip.xml :only (attr attr= text xml->)])
+  (:import [java.io ByteArrayInputStream]))
+
+;; API Endpoints
+(def marketstat "http://api.eve-central.com/api/marketstat")
+
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+;;; Getting around Clojure's braindead XML parser
+(defn string-to-stream [string]
+  (ByteArrayInputStream.
+   (.getBytes (.trim string))))
+
+(defn get-marketstat
+  "Retrieves data from the marketstat endpoint"
+  [typeid]
+  (let [return (client/get marketstat { :query-params { :typeid typeid } })]
+    (if (:body return)
+      (zip/xml-zip
+       (xml/parse (string-to-stream (:body return)))))))
+
+;;; Parsing
+;;; http://stackoverflow.com/questions/1194044/clojure-xml-parsing?rq=1
+;;; (xml-> zipped :marketstat :type (attr :id))
